632af953f040224dea049d8cf1859ac1d660c5dc
Hongyang Sun <sunhongyang@gmail.com>
2023-02-12 00:39:37 -0600

    new results

--- main.py
+++ main.py
@@ -5,10 +5,20 @@ from statistics import*
 import matplotlib.pyplot as plt
 
 
-compute_and_save('regular','Results_V2/',5)
+# compute_and_save('regular','Results_V2/Regular/',30)
+# compute_and_save('density','Results_V2/Density/',30)
+# compute_and_save('fat','Results_V2/Fat/',30)
+# compute_and_save('jump','Results_V2/Jump/',10)
 
-display_results('regular','Results_V2/')
+# compute_and_save('n','Results_V2/n/',30)
+# compute_and_save('p','Results_V2/P/',30)
 
+# display_results('regular','Results_V2/Regular/')
+# display_results('density','Results_V2/Density/')
+# display_results('fat','Results_V2/Fat/')
+display_results('jump','Results_V2/Jump/')
+# display_results('n','Results_V2/n/')
+# display_results('p','Results_V2/P/')
 
 
 
