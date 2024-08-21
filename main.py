9ab27e5420e9a25a92ee5556237b14829b49eb4c
Mary Jeevana Pudota <pjscience46@gmail.com>
2024-08-21 12:39:00 -0500

    Heat Maps for first algo

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ import itertools
 import random
 
 nb_iterations = 10
-mu_values = np.arange(0.15, 0.9, 0.05)
-beta_values = np.arange(1, 5, 0.05)
+mu_values = np.arange(0.65, 0.71, 0.05)
+beta_values = np.arange(3.55, 5, 0.05)
 # mu_values = [0.20]
 # beta_values = [1.80]
 
