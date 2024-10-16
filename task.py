da145f2641705713de739645d7258a9eb92efcf1
Mary Jeevana Pudota <pjscience46@gmail.com>
2024-10-15 23:38:31 -0500

    updated

--- task.py
+++ task.py
@@ -129,6 +129,34 @@ class Task:
     def get_p_max(self, P, speedup_model: Model):
         """"Allocating more than p_max processors to the task will no longer decrease its execution time"""
         return speedup_model.p_max(self, P)
+    
+    def count_digits(variable):
+        return sum(1 for char in str(variable) if char.isdigit())
+    def restrict_digi_count(w_str,count):
+        w_str = str(w_str) 
+        # Split the string into integer and decimal parts
+        if '.' in w_str:
+            integer_part, decimal_part = w_str.split('.')
+        else:
+            integer_part, decimal_part = w_str, ''
+
+        # Count total digits (ignoring the decimal point)
+        total_digits = len(integer_part) + len(decimal_part)
+
+        # Restrict to 10 significant digits
+        if total_digits > count:
+            if len(integer_part) >= count:
+                # If the integer part has 10 or more digits, keep only the first 10
+                w_str = integer_part[:count]
+            else:
+                # Otherwise, keep the integer part and trim the decimal part
+                digits_needed = count - len(integer_part)
+                w_str = integer_part + decimal_part[:digits_needed]
+
+        # Convert back to a float or int as needed
+        w = float(w_str) if '.' in w_str else int(w_str)
+
+        return w
 
     def allocate_processor_algo(self, P, mu_tild, alpha,Beta1, speedup_model: Model,version):
         """
@@ -147,23 +175,6 @@ class Task:
         d = self.get_d()
         c = self.get_c()
 
-        # p_max = self.get_p_max(P, speedup_model)
-        # t_min = self.get_minimum_execution_time(p_max, speedup_model)
-        # a_min = self.get_minimum_area(1, speedup_model)#execution_time * p = a
-       
-        
-        # Alpha_min = inf
-        # final_nb_processors = -1
-        # Upper_range = ceil(mu_tild * P)
-        # for i in range(1, p_max  + 1):
-        #     Alpha = self.get_area(i, speedup_model) / a_min[0]
-        #     Beta = self.get_execution_time(i, speedup_model) / t_min[0]
-
-        #     if Beta < (1 - 2 * mu_tild) / (mu_tild * (1 - mu_tild)):
-        #         if Alpha < Alpha_min:
-        #             Alpha_min = Alpha
-        #             final_nb_processors = i 
-
         p_max = self.get_p_max(P, speedup_model)
         t_min = self.get_execution_time(p_max, speedup_model)
         a_min = self.get_minimum_area(1, speedup_model)
@@ -173,9 +184,12 @@ class Task:
             final_nb_processors = -1
             upper_range = ceil(mu_tild * P)
             for i in range(1, p_max + 1):   
-                AR = self.get_area(i, speedup_model) / a_min[0]
+                get_area = self.get_area(i, speedup_model)
+                dig_count1 = Task.count_digits(w) 
+                dig_count2 = Task.count_digits(get_area)   
+                task_area = Task.restrict_digi_count(get_area,dig_count1)
+                AR = task_area / a_min[0]
                 TR = self.get_execution_time(i, speedup_model) / t_min
-                
                 if TR >=1 and TR <= Beta1:
                     if AR < Alpha_min:
                         Alpha_min = AR
