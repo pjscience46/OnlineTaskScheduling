db0d7e3a5f2e6fd16df25ad0931bece5b4de7ac2
Mary Jeevana Pudota <pjscience46@gmail.com>
2025-01-05 22:44:06 -0600

    update

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 import itertools
 import random
 
-mu_values = list(map(float, np.arange(0.3, 0.92, 0.1)))
-paramter_values = list(map(float, np.arange(1, 3.2, 0.5)))
+mu_values = list(map(float, np.arange(0.1, 0.92, 0.1)))
+paramter_values = list(map(float, np.arange(1, 5.2, 0.5)))
 
 version = int(input("Enter algorithm version number[0-MAST, 1-MTSA, 2-MTPA] : "))
 if version == 0:
